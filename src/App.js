9852be44c4befd09b4351d8246faece5e4538a1e
NicoleS1312 <102580592+nicole1312@users.noreply.github.com>
2022-06-20 14:04:49 +0100

    Created new component Weather

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Forecast from "./Forecast";
 
 function App() {
   return (
     <div className="App">
       <h1> Hello</h1>
+      <Forecast />
       <footer>
         <a
           href="https://github.com/nicole1312/forecast-react-app"
---
+++ src/Forecast.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Forecas() {
+  return (
+    <div className="Forecast">
+      <h3>Forecast</h3>
+    </div>
+  );
+}
