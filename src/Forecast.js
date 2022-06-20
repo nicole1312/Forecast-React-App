c214a225768d900c9944981b9d95b171df239d1d
NicoleS1312 <102580592+nicole1312@users.noreply.github.com>
2022-06-20 15:15:35 +0100

    Added forecast day data

--- src/Forecast.js
+++ src/Forecast.js
@@ -4,7 +4,7 @@ import "./Forecast.css";
 export default function Forecast() {
   return (
     <div className="Forecast">
-      <h3>TheWeatherHub</h3>
+      <h1>TheWeatherHub</h1>
       <div className="container">
         <form>
           <div className="row">
@@ -20,6 +20,33 @@ export default function Forecast() {
             </div>
           </div>
         </form>
+      </div>
+      <div className="weather">
+        <div className="row">
+          <div className="col-sm-6 ">
+            <p className="date">Monday 14:49</p>
+            <h3 className="city text-capitalize">london</h3>
+            <ul>
+              <li className="description ">clear sky</li>
+              <li>19°C</li>
+              <span>⇧20°C</span>
+              <span>⇩10°C</span>
+              <li>
+                <img
+                  src=" http://openweathermap.org/img/wn/01d@2x.png"
+                  alt="weather-icon"
+                />
+              </li>
+            </ul>
+          </div>
+          <div className="col-sm-6">
+            <ul className="weather-analysis">
+              <li>Humidity: 45%</li>
+              <li>Wind: 9mph</li>
+              <li>Pressure: 1019mb</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
