bf3bef25c3225c043be07c14493440a13e7d57e9
NicoleS1312 <102580592+nicole1312@users.noreply.github.com>
2022-06-20 16:44:38 +0100

    Added basic styling

--- src/Forecast.js
+++ src/Forecast.js
@@ -12,7 +12,8 @@ export default function Forecast() {
               <input
                 type="search"
                 placeholder="Search for a city..."
-                className="form-control"
+                autoFocus="on"
+                className="form-control form-search"
               />
             </div>
             <div className="col-sm-2">
@@ -26,24 +27,37 @@ export default function Forecast() {
           <div className="col-sm-6 ">
             <p className="date">Monday 14:49</p>
             <h3 className="city text-capitalize">london</h3>
-            <ul>
-              <li className="description ">clear sky</li>
-              <li>19°C</li>
-              <span>⇧20°C</span>
-              <span>⇩10°C</span>
-              <li>
-                <img
-                  src=" http://openweathermap.org/img/wn/01d@2x.png"
-                  alt="weather-icon"
-                />
-              </li>
-            </ul>
+            <h4 className="description ">clear sky</h4>
+
+            <p>
+              <strong>19</strong>°C
+            </p>
+            <div className="degrees">
+              <span>
+                <strong>⇧20</strong>°C
+              </span>
+              <span>
+                <strong>⇩10</strong>°C
+              </span>
+            </div>
+            <p>
+              <img
+                src=" http://openweathermap.org/img/wn/01d@2x.png"
+                alt="weather-icon"
+              />
+            </p>
           </div>
           <div className="col-sm-6">
             <ul className="weather-analysis">
-              <li>Humidity: 45%</li>
-              <li>Wind: 9mph</li>
-              <li>Pressure: 1019mb</li>
+              <li>
+                <strong>Humidity:</strong> 45%
+              </li>
+              <li>
+                <strong>Wind:</strong> 9mph
+              </li>
+              <li>
+                <strong>Pressure:</strong> 1019mb
+              </li>
             </ul>
           </div>
         </div>
